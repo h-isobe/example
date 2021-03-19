e734b3e0d54b69508556b951d9509a3b48566da1
isobehiroki <h.sep23@gmail.com>
2021-03-19 18:26:11 +0900

    [Add]練習問題11-2

---
+++ src/listener/NoAppScopeListener.java
@@ -0,0 +1,44 @@
+//練習問題11-2 アプリケーションスコープの禁止
+package listener;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class NoAppScopeListener
+ *
+ */
+@WebListener
+public class NoAppScopeListener implements ServletContextAttributeListener {
+
+    /**
+     * Default constructor.
+     */
+    public NoAppScopeListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+     */
+    public void attributeAdded(ServletContextAttributeEvent scae)  {
+         // TODO Auto-generated method stub
+    	System.out.println("警告:格納は禁止されています");
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+     */
+    public void attributeRemoved(ServletContextAttributeEvent scae)  {
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+     */
+    public void attributeReplaced(ServletContextAttributeEvent scae)  {
+         // TODO Auto-generated method stub
+    }
+
+}
